b0923da204774df1822f8e1f20d118cd5e2bb689
Artem <dom1nuk@yandex.ru>
2020-12-02 16:36:08 +0300

    Created project

---
+++ iTunesAlbums/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iTunesAlbums
+//
+//  Created by Артем  Емельянов  on 02.12.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
